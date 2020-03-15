3534ad52e888a10c573468248be521e925b05ffd
KamilPlekaniec <kp054467@student.ath.edu.pl>
2020-03-15 16:12:02 +0100

    Lab3 pliki

---
+++ Lab3/Lab3/Zajecia.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Lab3
+{
+    public class Zajecia
+    {
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(255)]
+        [Column("Nazwa przedmiotu")]
+        public string Nazwa { get; set; }
+        public DateTime GodzinaRozpozcecia { get; set; }
+
+    }
+}
