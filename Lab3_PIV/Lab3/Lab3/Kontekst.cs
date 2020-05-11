b5c2c90e33fd293c29b6bbb2b03c8c9670ae45c7
KamilPlekaniec <kp054467@student.ath.edu.pl>
2020-05-11 21:43:39 +0200

    Update 2

---
+++ Lab3_PIV/Lab3/Lab3/Kontekst.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab3
+{
+    public class Kontekst : DbContext
+    {
+        //pierwszy sposob
+        public DbSet<Zajecia> Zajecias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
+        //drugi sposob fluent 
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    modelBuilder
+        //        .Entity<Zajecia>()
+        //        .Property(x => x.Nazwa)
+        //        .HasMaxLength(255)
+        //        .IsRequired()
+        //        .HasColumnName("NazwaFluent");
+
+        //}
+    }
+}
